006e71d3bb10c5260b1c488c547a6ffe932becaa
alvinhizra <alvinhizra9@gmail.com>
2020-02-10 11:37:25 +0700

    'goldycheckdong'

---
+++ day-1.py
@@ -0,0 +1,45 @@
+# Variable
+
+# nama = 'uga' #string
+# tanggal_lahir = 32 #numerical
+# status = True #boolean
+
+# print(type(nama))
+# print(type(tanggal_lahir))
+# print(type(status))
+# =============================================================
+
+# nama = input("Siapa nama kamu? :")
+# umur = input('Maukkan umur :')
+# jenisKelamin = input('Laki-laki / Perempuan : ')
+# pekerjaan = input('Pekerjaan kamu? ')
+
+# print(nama)
+# print(umur)
+# print(jenisKelamin)
+# print(pekerjaan)
+# =============================================================
+
+# Operasi aritmatika
+# * / + -
+
+# pertambahan = 25*25
+# pengurangan = 25*23
+# pembagian = 2/4
+# perkalian = 8*2
+# modulus = 7%2
+
+# print(modulus)
+
+# def lower_limit():
+#     return 68.5
+
+# def upper_limit():
+#     return 74.5
+
+# def remove_outlier(list):
+#     list_outlier = []
+#     for item in list:
+#         if item > 64.5 or item < 75.5:
+#             list_outlier.append(item)
+#     return list_outlier
